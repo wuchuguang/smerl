981bf4b72416780fdf42352b1c06e0cb7b5e020f
yarivvv <yarivvv@8fc30806-7e1b-0410-991b-dd3963df6c7b>
2006-08-29 13:49:48 +0000

    Made the source path discovery more robust and changed the license to the MIT license.

--- smerl.erl
+++ smerl.erl
@@ -84,30 +84,27 @@
 %%   
 %% @author Yariv Sadan <yarivvv@gmail.com> [http://yarivsblog.com]
 
-%% * Copyright (c) 2006, Yariv Sadan
-%% * All rights reserved.
-%% * Redistribution and use in source and binary forms, with or without
-%% * modification, are permitted provided that the following conditions are met:
-%% *
-%% *     * Redistributions of source code must retain the above copyright
-%% *       notice, this list of conditions and the following disclaimer.
-%% *     * Redistributions in binary form must reproduce the above copyright
-%% *       notice, this list of conditions and the following disclaimer in the
-%% *       documentation and/or other materials provided with the distribution.
-%% *     * Neither the name of the Yariv Sadan nor the
-%% *       names of its contributors may be used to endorse or promote products
-%% *       derived from this software without specific prior written permission.
-%% *
-%% * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
-%% * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-%% * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-%% * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
-%% * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-%% * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-%% * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-%% * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-%% * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-%% * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%% Copyright (c) 2006 Yariv Sadan
+%%
+%% Permission is hereby granted, free of charge, to any person
+%% obtaining a copy of this software and associated documentation
+%% files (the "Software"), to deal in the Software without restriction,
+%% including without limitation the rights to use, copy, modify, merge,
+%% publish, distribute, sublicense, and/or sell copies of the Software,
+%% and to permit persons to whom the Software is furnished to do
+%% so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included
+%% in all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+%% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+%% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+%% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+%% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+%% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 -module(smerl).
 -author("Yariv Sadan (yarivvv@gmail.com").
@@ -172,16 +169,24 @@ new(ModuleName) when is_atom(ModuleName) ->
 for_module(ModuleName) when is_list(ModuleName) ->
     new(list_to_atom(ModuleName));
 for_module(ModuleName) when is_atom(ModuleName) ->
-    case code:which(ModuleName) of
-	Path when is_list(Path) ->
-	    case get_forms(ModuleName, Path) of
-		{ok, Forms} ->
-		    mod_for_forms(Forms);
-		_Other ->    
+    [_Exports, _Imports, _Attributes,
+     {compile, [_Options, _Version, _Time, {source, Path}]}] =
+	ModuleName:module_info(),
+    case for_file(Path) of
+	{ok, Mod} ->
+	    Mod;
+	_Err ->
+	    case code:which(ModuleName) of
+		Path1 when is_list(Path1) ->
+		    case get_forms(ModuleName, Path1) of
+			{ok, Forms} ->
+			    mod_for_forms(Forms);
+			_Other ->
+			    {error, invalid_module}
+		    end;
+		_Err ->
 		    {error, invalid_module}
-	    end;
-	Err ->
-	    {error, Err}
+	    end
     end.
 
 %% @doc Create a MetaMod object for a module described in the given source
@@ -192,7 +197,7 @@ for_module(ModuleName) when is_atom(ModuleName) ->
 for_file(SrcFilePath) ->
     case epp:parse_file(SrcFilePath, [], []) of
 	{ok, Forms} ->
-	    mod_for_forms(Forms);
+	    {ok, mod_for_forms(Forms)};
 	_err ->
 	    {error, invalid_module}
     end.
